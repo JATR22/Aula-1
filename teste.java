130757ad59ca0004195be17757bf224b1f89c93f
JATR22 <97111949+JATR22@users.noreply.github.com>
2022-01-04 15:32:54 +0000

    Create teste.java
    Teste aula 1

---
+++ teste.java
@@ -0,0 +1,42 @@
+public class Teste {
+	public static void main(String[] args) 
+	{
+	System.out.println("Escreva um programa que peça ao utilizador dois números inteiros.");
+	
+	Scanner consola = new Scanner(System.in);
+	System.out.println("Escreva dois números inteiros. ");
+	int maiorNumero = consola.nextInt();
+	
+	
+	System.out.println("Escreva o seu primeiro número:");
+	Scanner in = new Scanner(System.in);
+	int numero = in.nextInt();
+	System.out.println("Escreva o seu segundo número:");
+	Scanner in2 = new Scanner(System.in);
+	int numero2 = in2.nextInt();
+	
+	if (numero>numero2)
+	{
+		System.out.println("O número maior é:" + numero);
+	}
+	else
+	{
+		System.out.println("O número maior é:" + numero2);
+		if (numero==numero2)
+		{
+		System.out.println();
+		in. close();
+		in2.close();
+		}
+		else 
+		{
+		System.out.println("O número maior é:" + numero2);
+		}
+		for (int i = 0; i <= maiorNumero; i++)
+		{
+		System.out.println(i);
+		}
+	consola.close();
+		}
+	}
+}
